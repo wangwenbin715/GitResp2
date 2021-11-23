fadaebaf1657fdb6c32bff05402e90ffa9cf2b03
wangjc <1591960594@qq.com>
2021-11-23 20:59:29 +0800

    Person中添加了age信息

---
+++ TestModule01/src/com/msb/test/Person.java
@@ -0,0 +1,11 @@
+package com.msb.test;
+
+/**
+ * @Auther: Mr.Wang
+ * @Date: 2021/11/23 - 11 - 23 - 20:55
+ * @Description: com.msb.test
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
